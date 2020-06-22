171aef3b351b5eac38e4900463628ca1678359de
gameaccount0419 <66716011+gameaccount0419@users.noreply.github.com>
2020-06-22 20:16:46 +0800

    Add files via upload

---
+++ Pure CSS Slider Complete/product.js
@@ -0,0 +1,22 @@
+{
+	"product": [{
+			"name": "iphone 11",
+			"brand": "apple",
+"description": "iphone 11",
+			"price": "280"
+
+		},
+		{
+			"name": "iphone 11",
+			"brand": "apple",
+"description": "iphone 11",
+			"price": "280"
+		},
+		{
+			"name": "iphone 11",
+			"brand": "apple",
+" description": "iphone 11",
+			"price": "280"
+		}
+	]
+}
